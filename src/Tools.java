2e88299ad70c05d8a6d0139f8ec4c6c5ed20ee2c
deval <deval.patel5@dcmail.ca>
2024-03-20 14:50:28 -0400

    noothinf

--- src/Tools.java
+++ src/Tools.java
@@ -1,9 +1,49 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Tools {
-    private static final Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
     public static int getInt(String message){
+        int num = 0;
         System.out.print(message);
-        return scanner.nextInt();
+        while (true){
+            try {
+                num = scanner.nextInt();
+                return num;
+            } catch (InputMismatchException e){
+                System.out.println("Error!!! Enter a valid number...");
+                scanner.next();
+            }
+        }
+    }
+    public static double getDouble(String message){
+        double num = 0;
+        System.out.print(message);
+        while (true){
+            try {
+                num = scanner.nextDouble();
+                return num;
+            } catch (InputMismatchException e){
+                System.out.println("Error!!! Enter a valid number...");
+                scanner.next();
+            }
+        }
+    }
+    public static String getString(String message){
+        System.out.print(message);
+        return scanner.nextLine();
+    }
+    public static String getServiceCode(String message){
+        String input;
+        System.out.print(message);
+        while (true){
+            try {
+                input = scanner.nextLine().trim();
+                return input;
+            } catch (InputMismatchException e){
+                System.out.println("Error!!! Enter a valid input...");
+                scanner.next();
+            }
+        }
     }
 }
--- src/Useless.java
+++ src/Useless.java
@@ -1,18 +1,24 @@
+import java.io.StringReader;
 
-
-public class Useless{
+public class Useless {
     public static void main(String[] args){
-        int n = Tools.getInt("");
-        int answer = fun(n);
-        System.out.println(answer%1000000007);
-
+        int[] array = {5,6,3,2,4,7,8,9,1,10};
+        selectionSort(array);
+        printArray(array);
     }
-    private static int fun(int n){
-        if (n == 0) return 2;
-        else if (n == 1) return 5;
-        else if (n == 2) return 9;
-        else if (n == 3) return 7;
-        else if (n % 2 == 0) return (fun(n-2)*(n-2)) + (fun(n-4)*(n-4));
-        else return (fun(n-1)*(n-1)) + (fun(n-3)*(n-3));
+    private static void selectionSort(int[] array){
+        for (int i = 0; i < array.length -1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i]){
+                    array[i] = array[j];
+                    array[j] = array[i];
+                }
+            }
+        }
+    }
+    private static void printArray(int[] array){
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
     }
 }
